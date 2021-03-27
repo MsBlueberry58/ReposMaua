41771d7ba1f082d1ecf30efd4d7fef1e86135daa
MsBlueberry58 <brunagguedes4@gmail.com>
2021-03-27 10:39:38 -0300

    Desenvolvendo observações + instalando uuid

---
+++ ArquiteturaSistemas/Gerenciador_de_lembretes/observacoes/index.js
@@ -0,0 +1,56 @@
+const express = require ('express');
+
+const bodyParser = require("body-parser");
+
+// Import nomeado, está atribuindo um apelido ao atributo, um nome 
+// para esse contexto
+const {v4: uuid} = require ('uuid');
+
+const app = express();
+app.use(bodyParser.json);
+
+// Definindo a base de dados de memória volátil
+/*
+    {
+        1: {
+            {id: a, texto: Entre 14h e 15h},
+            {id: b, texto: Outra obs}
+        },
+
+        2: {
+            {id: a, texto: blabalba},
+            {id: b, texto: pipippopopo}
+        }
+    }
+ */
+const observacoesPorLembreteID = {}
+
+app.put('/lembretes/:id/observacoes', (req, res) => {
+
+    // Gerando um id com o uuid v4; vai enviar o id de lembrete e depois a observação
+    const idObs = uuidv4();
+
+    // Extraindo o texto
+    const { texto } = req.body;
+
+    // Está acessando o que existe na url, essa chave está associada ao valor passado 
+    // para o :id; olhando para a base tentando pegar um objeto associado a esse id, sendo
+    // que pode existir ou não (vazio ou não)
+    const observacoesDoLembreteAtual = observacoesPorLembreteID[req.params.id] || [];
+
+    // Uma chave denominada texto associada à uma variável chamada texto pode ser
+    // reduzida apenas à palavra "texto"
+    observacoesDoLembreteAtual.push({id: idObs, texto});
+
+    // Se não existia, é preenchida; se existia, é atualizada
+    observacoesDoLembreteID[req.params.id] = observacoesDoLembreteAtual;
+    
+    // CREATED
+    res.status(201)
+});
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+
+});
+
+app.listen(5000, () => console.log ("Microserviço observações, Porta 5000"))

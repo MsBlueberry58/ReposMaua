c8edb2860d237906f3d4c54ead072169cad67924
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2021-06-05 18:44:29 -0300

    tirando console.log

--- ArquiteturaSistemas/Projeto_Vacina/alerta_vacina/index.js
+++ ArquiteturaSistemas/Projeto_Vacina/alerta_vacina/index.js
@@ -67,7 +67,6 @@ app.get('/idades', (req, res) => {
 app.put('/alertar',  (req, res) => {
 
   faixa = req.body.idade;
-  console.log(faixa);
 
   for (id = 0; id < Object.keys(baseIdades).length; id++) {
 

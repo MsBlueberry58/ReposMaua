6ecc85e57106e60b7407a8baa9cbb779528aafc4
VitorMartin <vitor.martin.simoni@gmail.com>
2021-06-12 16:34:40 -0300

    Consertando referencias .env

--- ArquiteturaSistemas/Projeto_Vacina/custom_notif/index.js
+++ ArquiteturaSistemas/Projeto_Vacina/custom_notif/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const axios = require('axios');
-require('dotenv').config({path: 'C:/Users/leafe/Documents/ReposMaua/ArquiteturaSistemas/Projeto_Vacina/.env'});
+require('dotenv').config({path:__dirname+'/../../.env'});
 
 
 
